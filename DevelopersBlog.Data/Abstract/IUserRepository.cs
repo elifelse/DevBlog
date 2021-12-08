853ba4fc82dc22125b5577824452aa832ebcd507
elifelse <elifgsavukcam@gmail.com>
2021-12-08 13:54:10 +0300

    IEntityRepository implement in Data Layer

---
+++ DevelopersBlog.Data/Abstract/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevelopersBlog.Entities.Concrete;
+using DevelopersBlog.Shared.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopersBlog.Data.Abstract
+{
+    public interface IUserRepository : IEntityRepository<User>
+    {
+    }
+}
