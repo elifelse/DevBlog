e29a22a16286d6feeb3f3c6109d1edc781fb8c20
elifelse <elifgsavukcam@gmail.com>
2021-12-08 15:48:14 +0300

    Mapping with Fluent API

--- DevelopersBlog.Data/Concrete/EntityFramework/Contexts/DevelopersBlogContext.cs
+++ DevelopersBlog.Data/Concrete/EntityFramework/Contexts/DevelopersBlogContext.cs
@@ -1,4 +1,5 @@
-﻿using DevelopersBlog.Entities.Concrete;
+﻿using DevelopersBlog.Data.Concrete.EntityFramework.Mappings;
+using DevelopersBlog.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,15 @@ namespace DevelopersBlog.Data.Concrete.EntityFramework.Contexts
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=ProgrammersBlog; Trusted_Connection=True; Connect Timeout=30; MultipleActiveResultSets=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new CommentMap());
+            modelBuilder.ApplyConfiguration(new RoleMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
         }
     }
 }
