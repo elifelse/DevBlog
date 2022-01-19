53ba27aa9840e7f22272e3e966348431c7b96928
elifelse <elifgsavukcam@gmail.com>
2022-01-19 17:59:51 +0300

    Added ServiceCollectionExtensions class and created LoadMyServices method

---
+++ DevelopersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using DevelopersBlog.Data.Abstract;
+using DevelopersBlog.Data.Concrete;
+using DevelopersBlog.Data.Concrete.EntityFramework.Contexts;
+using DevelopersBlog.Services.Abstract;
+using DevelopersBlog.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopersBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<DevelopersBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+            return serviceCollection;
+        }
+    }
+}
