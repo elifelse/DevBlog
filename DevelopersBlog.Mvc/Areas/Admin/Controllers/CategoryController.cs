8d712136af91e3a8e496493e45dc4a9a63877f5e
elifelse <elifgsavukcam@gmail.com>
2022-02-07 19:00:41 +0300

    preparation for ajax

--- DevelopersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ DevelopersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using DevelopersBlog.Services.Abstract;
+﻿using DevelopersBlog.Entities.Dtos;
+using DevelopersBlog.Mvc.Areas.Admin.Models;
+using DevelopersBlog.Services.Abstract;
+using DevelopersBlog.Shared.Utilities.Extensions;
 using DevelopersBlog.Shared.Utilities.Results.ComplexTypes;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -21,9 +24,18 @@ namespace DevelopersBlog.Mvc.Areas.Admin.Controllers
             var result = await _categoryService.GetAll();
             return View(result.Data);
         }
+        [HttpGet]
         public IActionResult Add()
         {
             return PartialView("_CategoryAddPartial");
+        }
+        [HttpPost]
+        public async IActionResult Add(CategoryAddDto categoryAddDto)
+        {
+            var categoryAjaxModel = new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto),
+            };
         }
     }
 }
